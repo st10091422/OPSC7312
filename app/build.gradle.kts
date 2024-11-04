76ffafddc5f0acff41f6035916d4d57048dcedc4
Thato <st10067544@vcconnect.edu.za>
2024-11-04 21:55:40 +0200

    Calendar Fragment + API upgrade.
    Co-Authored-By: st10091422 <130764382+st10091422@users.noreply.github.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.opsc.opsc7312poe"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.opsc.opsc7312poe"
@@ -52,12 +52,13 @@ dependencies {
     testImplementation(libs.androidx.core.testing)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.9.1")
-    implementation("androidx.preference:preference:1.2.0")
-    implementation("com.google.android.gms:play-services-auth:21.2.0")
-    implementation("androidx.biometric:biometric:1.1.0")
-    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
-    implementation("androidx.work:work-runtime-ktx:2.8.1")
+
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+    implementation(libs.logging.interceptor)
+    implementation(libs.androidx.preference)
+    implementation(libs.play.services.auth)
+    implementation(libs.androidx.biometric)
+    implementation(libs.mpandroidchart)
+    implementation(libs.androidx.work.runtime.ktx)
 }
